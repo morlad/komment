39b54539d4fddec047cb70971864f1cfc3d3c63c
morlad <morlad@morlad.at>
2018-11-09 19:14:29 +0000

    Prepare backend for different actions

--- backend.go
+++ backend.go
@@ -4,11 +4,24 @@ import (
   "fmt"
   "os"
   "syscall"
-  "io/ioutil"
   "net/http"
   "net/http/cgi"
-  "encoding/json" // for config file
+  "encoding/json"
+  "math/rand"
 )
+
+
+const LOG_PATH = "komment.log"
+const LOG_MODE = 0664
+const LOG_FLAG = os.O_WRONLY | os.O_CREATE | os.O_SYNC
+
+const COMMENT_EXT = "_comments.json"
+const COMMENT_FLAG = os.O_APPEND | os.O_CREATE | os.O_WRONLY
+const COMMENT_MODE = 0664
+
+// in seconds
+const COOKIE_EDIT_WINDOW = 300
+const COOKIE_PREFIX = "komment_ownership_"
 
 func emit_status_500(msg string) {
 
@@ -18,39 +31,65 @@ func emit_status_500(msg string) {
   fmt.Printf("%s\r\n", msg)
 }
 
+
 func handler(w http.ResponseWriter, r *http.Request) {
 
-  w.Header().Set("Content-Type", "text/json")
-  w.WriteHeader(200)
+  request := r.FormValue("r")
+  komment_id := r.FormValue("komment_id")
 
-  r.ParseForm()
+  // append new comment
+  if request == "a" {
 
-  data := make(map[string]string)
-  data["komment_id"] = r.Form["komment_id"][0]
-  data["comment"] = r.Form["comment"][0]
-  data["name"] = r.Form["name"][0]
+    data := make(map[string]string)
+    data["comment"] = r.FormValue("comment")
+    data["name"] = r.FormValue("name")
+    data["stamp"] = fmt.Sprintf("%x", rand.Uint64())
 
-  b, err := json.Marshal(data)
-  if err == nil {
-    ioutil.WriteFile("comments_"+data["komment_id"]+".json", b, 0644)
+    b, err := json.Marshal(data)
+    if err != nil {
+      panic(err.Error())
+    }
+    f, err := os.OpenFile(komment_id + COMMENT_EXT, COMMENT_FLAG, COMMENT_MODE)
+    if err != nil {
+      panic(err.Error())
+    }
+    f.Write(b)
+
+    w.Header().Set("Content-Type", "text/json")
+
+    var cookie http.Cookie
+    cookie.Name = COOKIE_PREFIX + data["stamp"]
+    cookie.Value = "true"
+    cookie.MaxAge = COOKIE_EDIT_WINDOW
+    http.SetCookie(w, &cookie)
+
+    w.WriteHeader(200)
+    fmt.Fprintln(w, "{ \"result\": \"ok\" }")
+
+  // count
+  } else if request == "c" {
+  // list all comments
+  } else if request == "l" {
+  // edit comment
+  } else if request == "e" {
+  // no request type -> error
   } else {
-    b = []byte(err.Error())
-    ioutil.WriteFile("comments_"+data["komment_id"]+".json", b, 0644)
+    emit_status_500("Invalid Request")
   }
-
-  fmt.Fprintln(w, "{ \"result\": \"ok\" }")
 }
+
 
 func main() {
 
   // redirect <stderr> to logfile
-  logFile, err := os.OpenFile("komment.log", os.O_WRONLY | os.O_CREATE | os.O_SYNC, 0664)
+  logFile, err := os.OpenFile(LOG_PATH, LOG_FLAG, LOG_MODE)
   if err != nil {
     emit_status_500(err.Error())
     return
   }
   syscall.Dup2(int(logFile.Fd()), 2)
 
+  // serve
   err = cgi.Serve(http.HandlerFunc(handler))
   if err != nil {
     emit_status_500(err.Error())
